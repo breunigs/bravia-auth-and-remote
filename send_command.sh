6e28f23a4f532d53e9223840ae23aef0224fb1b6
Giorgio Ravera <47370115+xraver@users.noreply.github.com>
2023-02-24 18:06:01 +0100

    Update send_command.sh (#16)
    * Update send_command.sh
    
    Propose to pass the pin as variable to make it generic
    
    * Update README.md

--- send_command.sh
+++ send_command.sh
@@ -2,15 +2,14 @@
 
 set -e
 
-if [ "$1" = "" ] || [ "$2" = "" ]; then
-  echo "Usage: $0 <TV_IP> <IRCC_COMMAND>"
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ]; then
+  echo "Usage: $0 <TV_IP> <TV_PIN> <IRCC_COMMAND>"
   exit 1
 fi
 
-cmd="<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:X_SendIRCC xmlns:u=\"urn:schemas-sony-com:service:IRCC:1\"><IRCCCode>$2</IRCCCode></u:X_SendIRCC></s:Body></s:Envelope>"
+cmd="<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:X_SendIRCC xmlns:u=\"urn:schemas-sony-com:service:IRCC:1\"><IRCCCode>$3</IRCCCode></u:X_SendIRCC></s:Body></s:Envelope>"
 
-
-code=$(curl -w "%{http_code}" --silent -XPOST http://$1/sony/IRCC -d "$cmd" -H 'Content-Type: text/xml; charset=UTF-8' -H 'SOAPACTION: "urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"' -H "X-Auth-PSK: 0000" -o /dev/null )
+code=$(curl -w "%{http_code}" --silent -XPOST http://$1/sony/IRCC -d "$cmd" -H 'Content-Type: text/xml; charset=UTF-8' -H 'SOAPACTION: "urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"' -H "X-Auth-PSK: $2" -o /dev/null )
 
 if [ "$code" = "200" ]; then
   echo "✓"
